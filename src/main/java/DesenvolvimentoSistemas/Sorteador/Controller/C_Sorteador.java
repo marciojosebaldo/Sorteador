58f3a83a58c6191a3da2c19507d472d0695974cb
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-11 20:31:22 -0300

    Add files via upload

--- src/main/java/DesenvolvimentoSistemas/Sorteador/Controller/C_Sorteador.java
+++ src/main/java/DesenvolvimentoSistemas/Sorteador/Controller/C_Sorteador.java
@@ -2,12 +2,23 @@ package DesenvolvimentoSistemas.Sorteador.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_Sorteador {
     @GetMapping("/")
     public String getHome() {
         return "Home/home";
+    }
+
+    @PostMapping("/")
+    public String calSorteador(@RequestParam("qtdeNumero") int qtdeNumero,
+                               @RequestParam("iniNumero") int iniNumero,
+                               @RequestParam("fimNumero") int fimNumero) {
+
+        return "x";
+
     }
 
 }
--- src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
+++ src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
@@ -2,6 +2,43 @@ package DesenvolvimentoSistemas.Sorteador.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 @Service
 public class S_Sorteador {
+
+    private int qtdeNumero;
+    private int iniNumero;
+    private int fimNumero;
+    private boolean crescente;
+    private boolean semRepeticao;
+
+    public S_Sorteador(int qtdeNumero, int iniNumero, int fimNumero, boolean crescente, boolean semRepeticao) {
+        this.qtdeNumero = qtdeNumero;
+        this.iniNumero = iniNumero;
+        this.fimNumero = fimNumero;
+        this.crescente = crescente;
+        this.semRepeticao = semRepeticao;
+    }
+
+
+    public List Sorteador(int qtdeNumero, int iniNumero, int fimNumero, boolean crescente, boolean semRepeticao) {
+        List<Integer>listaSorteio = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < qtdeNumero; i++) {
+            int valor = random.nextInt(fimNumero) + iniNumero;
+            listaSorteio.add(valor);
+
+            if (crescente == true) {
+                
+            }
+
+        }
+
+        return listaSorteio;
+    }
 }
