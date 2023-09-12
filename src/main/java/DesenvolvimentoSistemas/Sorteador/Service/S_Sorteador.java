841be2e6300ac81c749552d52df58a09aaad48aa
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-12 20:04:54 -0300

    Add files via upload

--- src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
+++ src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
@@ -7,20 +7,6 @@ import java.util.*;
 @Service
 public class S_Sorteador {
 
-//    private int qtdeNumero;
-//    private int iniNumero;
-//    private int fimNumero;
-//    private boolean crescente;
-//    private boolean semRepeticao;
-
-//    public S_Sorteador(int qtdeNumero, int iniNumero, int fimNumero, boolean crescente, boolean semRepeticao) {
-//        this.qtdeNumero = qtdeNumero;
-//        this.iniNumero = iniNumero;
-//        this.fimNumero = fimNumero;
-//        this.crescente = crescente;
-//        this.semRepeticao = semRepeticao;
-//    }
-
     public S_Sorteador() {
 
     }
