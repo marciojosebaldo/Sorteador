5778d96386cd8a9600ed6bae8568e81540579f0b
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-13 20:39:31 -0300

    S

--- src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
+++ src/main/java/DesenvolvimentoSistemas/Sorteador/Service/S_Sorteador.java
@@ -19,13 +19,28 @@ public class S_Sorteador {
 
         if (semRepeticao) {
             for (int i = 0; i < listaSorteio.length; i++) {
+                int auxiliar = listaSorteio[i];
                 for (int j = 0; j < listaSorteio.length; j++) {
+                    if (auxiliar == listaSorteio[j] & j != i) {
+                        auxiliar = random.nextInt(fimNumero) + iniNumero;
+                    } else {
+                        listaSorteio[i] = auxiliar;
+                    }
 
                 }
             }
         }
 
         if (crescente) {
+            for (int i = 0; i < listaSorteio.length; i++) {
+                for (int j = 0; j < listaSorteio.length; j++) {
+                    if (listaSorteio[j] < listaSorteio[i]) {
+                        int auxiliar = listaSorteio[j];
+                        listaSorteio[j] = listaSorteio[i];
+                        listaSorteio[i] = auxiliar;
+                    }
+                }
+            }
         }
 
         return listaSorteio;
