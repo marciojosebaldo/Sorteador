651957652b01484901dc879d4c68adc0c85e82da
marciojosebaldo <marciojosebaldo@gmail.com>
2023-09-15 19:04:42 -0300

    JS

--- src/main/resources/static/js/validaEnvio.js
+++ src/main/resources/static/js/validaEnvio.js
@@ -1,15 +1,38 @@
-function validaEnvio() {
+function validaEnvio() { {}
     let validador = true;
     let semRepeticao = $("#semRepeticao").val();
     let qtdeNumero = $("#qtdeNumero").val();
     let iniNumero = $("#iniNumero").val();
     let fimNumero = $("#fimNumero").val();
 
+    $("#mensagemErro").text("");
     if (((fimNumero - iniNumero) > qtdeNumero) && semRepeticao == true) {
         validador = false;
+        mensagem: "Oops! A quantidade de números é menor do que o total de números"
     }
 
-}
+    if(podeEnviar) {
+        $.ajax({
+            type: "POST",
+            url: "/resultado",
+            data: {
+                qtdeNumero: qtdeNumero,
+                iniNumero: iniNumero,
+                fimNumero: fimNumero
+            },
+            success: function(data) {
+                $("#mensagemErro").text("");
+                if(data.sucesso) {
+                    window.location.href = "/resultado";
+                }
+            },
+            error: function() {
+                $('#mensagemErro').append("Falha na comunicação com o servidor");
+            }
+            }
+        )};
+    }
+
 function campoVazio() {
     if (campo == '') {
         return true;
